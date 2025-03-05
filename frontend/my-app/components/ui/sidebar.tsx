62b4820402f487ef33ee6a6ae7a77847a4b741b0
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-03-05 15:56:52 -0300

    :sparkles:feature: remove the sidebar button

--- frontend/my-app/components/ui/sidebar.tsx
+++ frontend/my-app/components/ui/sidebar.tsx
@@ -258,21 +258,7 @@ function SidebarTrigger({
   const { toggleSidebar } = useSidebar()
 
   return (
-    <Button
-      data-sidebar="trigger"
-      data-slot="sidebar-trigger"
-      variant="ghost"
-      size="icon"
-      className={cn("h-7 w-7", className)}
-      onClick={(event) => {
-        onClick?.(event)
-        toggleSidebar()
-      }}
-      {...props}
-    >
-      <PanelLeftIcon />
-      <span className="sr-only">Toggle Sidebar</span>
-    </Button>
+    <div></div>
   )
 }
 
